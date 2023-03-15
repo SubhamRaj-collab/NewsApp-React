e2aa09c2ee67137fea48a7357cdf995a40e73462
Subham Raj <glorious10info@gmail.com>
2023-03-15 07:19:04 +0530

    News Rendered as cards in News App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 
 import React, { Component } from 'react'
+import Navbar from './Components/Navbar.js';
+import News from './Components/News';
 
 export default class App extends Component {
 
@@ -8,7 +10,9 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        Subham Raj is a {this.c}
+        {/* Subham Raj is a {this.c} */}
+        <Navbar/>
+        <News/>
       </div>
     )
   }
